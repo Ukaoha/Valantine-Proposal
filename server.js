89f8ae67689edfe77d3dcc155b97cd6fa1a8d150
chizoba ukaoha <chizoba.ukaoha@ethnoscyber.com>
2025-02-03 23:14:18 +0100

    commit

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ mongoose.connect(process.env.MONGO_URI, {
 const proposalRoutes = require('./routes/proposalRoutes');
 app.use('/api', proposalRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+module.exports = app;
